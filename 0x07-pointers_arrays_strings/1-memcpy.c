0f6068ce221f1f2ae62ff740fc6bfbe7faedd010
nut_cracker <98818039+MargaretKerubo@users.noreply.github.com>
2022-03-29 00:06:38 +0300

    updated task 1

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -18,7 +18,7 @@ char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	unsigned int index;
     	for (index = 0; index < n; index++)
-		*(destination + index) = *(source + index);
+		*(dest + index) = *(src + index);
 
 	return (dest);
 }

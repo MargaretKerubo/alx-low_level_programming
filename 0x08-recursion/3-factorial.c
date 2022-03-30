7fe44f47667e7e84af13a18172005726810b680d
nut_cracker <98818039+MargaretKerubo@users.noreply.github.com>
2022-03-30 17:12:10 +0300

    Updated tasks 3-6

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -8,7 +8,7 @@
 /**
  * factorial - Returns the factorial of a given number.
  * @n: The number to find the factorial of.
- * 
+ *
  * Return: If n > 0 - the factorial of n.
  *         If n < 0 - 1 to indicate an error
  */
@@ -21,7 +21,7 @@ int factorial(int n)
 
 	else if (n >= 0 && n <= 1)
 		return (1);
-	
+
 	result *= factorial(n - 1);
 
 	return (result);
--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -10,7 +10,7 @@ int is_prime_number(int n);
 
 /**
  * is_divisible - Checks if a number is divisible.
- * @num: The number. 
+ * @num: The number.
  * @div: The divisor.
  *
  * Return: If the number is divisible - 0.
@@ -20,17 +20,17 @@ int is_divisible(int num, int div)
 {
 	if (num % div == 0)
 		return (0);
-	
+
 	if (div == num / 2)
 		return (1);
-	
+
 	return (is_divisible(num, div + 1));
 }
 
 /**
  * is_prime_number - Checks if a number is prime.
  * @n: The number.
- * 
+ *
  * Return: If the integer is not prime - 0.
  *         If the number is prime - 1.
  */
@@ -40,9 +40,9 @@ int is_prime_number(int n)
 
 	if (n <= 1)
 		return (0);
-	
+
 	if (n >= 2 && n <= 3)
 		return (1);
-	
+
 	return (is_divisible(n, div));
 }

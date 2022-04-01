c0e27179a3913b122abb96d37f99d78f618e0730
MargaretKerubo <98818039+MargaretKerubo@users.noreply.github.com>
2022-04-02 00:45:24 +0300

    Created all .c files needed in mylib file

---
+++ 0x09-static_libraries/0-strcat.c
@@ -0,0 +1,37 @@
+/*
+ * File: 0-strcat.c
+ * Auth: Marga Kerubo
+ */
+
+#include "main.h"
+
+/**
+ *  _strcat - Concatenates the string pointed to by @src, including the
+ *          terminating null byte, to the end of the string pointed to by @dest
+ * @dest: A pointer to the string to be concatenated upon.
+ * @src: The source string to be appended to @dest.
+ *
+ * Return: A pointer to the destination string @dest.
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int i, c;
+
+	i = 0;
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+
+	c = 0;
+	while (src[c] != '\0')
+	{
+		dest[i] = src[c];
+		i++;
+		c++;
+	}
+
+	dest[i] = '\0';
+	return (dest);
+}
